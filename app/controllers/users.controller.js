f79ff3c5f44b6c9657ae2b8b4aad87c8860f457e
Thanikul Sriuthis <moremeng@dv4.biz>
2020-07-13 15:41:56 +0700

    users search

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,8 +1,10 @@
 const Query = require("../models/query.model")
 
 exports.listUser = (req, res) => {
+    console.log(req.params.name);
+    // console.log(req.query.m);
 
-    let sqlstm = `SELECT * FROM [dbo].[_MOREMENG_LIST_USERS]`
+    let sqlstm = `SELECT * FROM [dbo].[_MOREMENG_LIST_USERS] WHERE Name LIKE '%${req.params.name}%'`
     Query.executeQuery(sqlstm, res)
 }
 
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     const routes = require(`../controllers/users.controller`)
 
-    app.get('/users', routes.listUser)
+    app.get('/users/:name', routes.listUser)
     app.post('/users/:userid', routes.addUser)
 }
