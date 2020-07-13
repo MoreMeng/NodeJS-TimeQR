d06f38c94a09254c32f59c82827f1b1b1f3fa399
Thanikul Sriuthis <moremeng@dv4.biz>
2020-07-13 17:14:09 +0700

    search LINE id

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,10 +1,17 @@
 const Query = require("../models/query.model")
 
 exports.listUser = (req, res) => {
-    console.log(req.params.name);
+    // console.log(req.params.name);
     // console.log(req.query.m);
 
-    let sqlstm = `SELECT * FROM [dbo].[_MOREMENG_LIST_USERS] WHERE Name LIKE '%${req.params.name}%'`
+    let sqlstm = `SELECT * FROM [dbo].[_MOREMENG_LIST_USERS] WHERE LINE_ID IS NULL AND Name LIKE '%${req.params.name}%'`
+    Query.executeQuery(sqlstm, res)
+}
+
+exports.searchLineId = (req, res) => {
+    // console.log(req.body.lineId);
+
+    let sqlstm = `SELECT * FROM [dbo].[_MOREMENG_LIST_USERS] WHERE LINE_ID = '${req.body.lineId}'`
     Query.executeQuery(sqlstm, res)
 }
 
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -2,5 +2,6 @@ module.exports = app => {
     const routes = require(`../controllers/users.controller`)
 
     app.get('/users/:name', routes.listUser)
+    app.post('/search', routes.searchLineId)
     app.post('/users/:userid', routes.addUser)
 }
