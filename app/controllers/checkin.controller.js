152335dee4a22ddeb3ee1558f036d6fd2ff95223
Thanikul Sriuthis <dekitclub@gmail.com>
2020-07-14 16:09:33 +0700

    add userdistance

--- app/controllers/checkin.controller.js
+++ app/controllers/checkin.controller.js
@@ -5,7 +5,8 @@ exports.checkInOut = (req, res) => {
     // console.log(req.body.line_userid)
     let userid = req.body.userid;
     let usergps = req.body.usergps;
+    let userdistance = req.body.userdistance;
     // let userid = 326;
-    let sqlstm = `INSERT INTO CHECKINOUT ( Badgenumber, CHECKTIME, CHECKTYPE, VERIFYCODE, SENSORID, WorkCode, sn, gps) VALUES ('${userid}',GETDATE(),'I',0,3,1,'S','${usergps}');`
+    let sqlstm = `INSERT INTO CHECKINOUT ( Badgenumber, CHECKTIME, CHECKTYPE, VERIFYCODE, SENSORID, WorkCode, sn, gps, distance) VALUES ('${userid}',GETDATE(),'I',0,3,1,'S','${usergps}', '${userdistance}');`
     Query.executeQuery(sqlstm, res)
 }
